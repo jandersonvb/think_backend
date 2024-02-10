f22868f8ca85b772805ad08729f27b66fca602f0
jandersonvb <jandersonvb.dev@gmail.com>
2024-02-09 21:11:44 -0300

    feat: create resource devices

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { DevicesModule } from './devices/devices.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    DevicesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
