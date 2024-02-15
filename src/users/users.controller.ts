e5058da6e7d63b188acbf1c8b8a3e0d481813163
jandersonvb <jandersonvb.dev@gmail.com>
2024-02-14 22:29:42 -0300

    feat: create user DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,11 +3,11 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
-  @Post()
+  @Post('/signup')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
